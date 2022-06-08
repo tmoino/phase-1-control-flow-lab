b2a2f4ddba4aa4dababf0bac33e599cdde654c0e
tmoino <tony.moino@student.moringaschool.com>
2022-06-08 13:22:23 +0300

    assignment completed

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
+  // Write your code here
+  if (feet <= 400) {
+  return "This one is on me!";
+  } 
+  else if ( feet > 2000 && feet < 2499){
+    return "I will gladly take your thirty bucks.";
+  }
+  else if ( feet > 2500)
+    return "No can do."
+  }
+   
+function ternaryCheckCity(destination){
   // Write your code here!
+  var result = (destination === "NYC")? "Ok, sounds good." : "No go." ;
+  return result; 
 }
 
-function ternaryCheckCity(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+switch(tip){
+  case tip = "generous":
+    return "Thank you so much.";
+  case tip = "not as generous":
+    return "Thank you.";
+  default:
+    return "Bye."
 }
-
-function switchOnCharmFromTip(){
-  // Write your code here!
 }
